4a9537065335dfe8436e483d0c7bd2f11ecf49d4
Catrin <catrinhughes92@gmail.com>
2019-11-08 17:01:31 +0000

    new pet form created

--- controllers/pets_controller.rb
+++ controllers/pets_controller.rb
@@ -1,6 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/pet.rb')
+require_relative('../models/vet.rb')
+require_relative('../models/owner.rb')
 also_reload('../models/*')
 
 get '/pets' do
@@ -8,7 +10,19 @@ get '/pets' do
   erb(:'/pets/index')
 end
 
-get '/pet/:id' do
+get '/pets/new' do
+  @owners = Owner.all
+  @vets = Vet.all
+  erb(:'/pets/new')
+end
+
+get '/pets/:id' do
   @pet = Pet.find(params[:id])
   erb(:'/pets/show')
+end
+
+post '/pets' do
+  @pet = Pet.new(params)
+  @pet.save
+  redirect to (:'/pets')
 end
