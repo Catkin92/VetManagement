269fed0025f7deff4c61e238630375e0bd7be327
Catrin <catrinhughes92@gmail.com>
2019-11-12 14:13:34 +0000

    create new appointment form made

--- controllers/appointments_controller.rb
+++ controllers/appointments_controller.rb
@@ -9,3 +9,15 @@ get '/appointments' do
   @appointments = Appointment.all.sort { |a, b| a.date <=> b.date }
   erb(:'/appointments/index')
 end
+
+get '/appointments/new' do
+  @pets = Pet.all.sort { |a, b| a.name <=> b.name }
+  @vets = Vet.all.sort { |a, b| a.name <=> b.name }
+  erb(:'/appointments/new')
+end
+
+post '/appointments' do
+  app = Appointment.new(params)
+  app.save
+  redirect to '/appointments'
+end
